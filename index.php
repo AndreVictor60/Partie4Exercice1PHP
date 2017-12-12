253dc06769c51859227f045c86bd9a113374064d
AndreVictor60 <andre.victor60@free.fr>
2017-12-12 21:15:55 +0100

    fini

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Partie 4 Exercice 1</title>
+  </head>
+  <body>
+    <?php
+    function returnCondition(){
+      return true;
+    }
+    echo returnCondition();
+     ?>
+  </body>
+</html>
